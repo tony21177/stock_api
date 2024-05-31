e7c970d6d5306284be07823eb447f7fd58feb906
tony <tony@ying31.com>
2024-05-31 22:18:29 +0800

    修改build腳本

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @REM dotnet build  --source .\stock_api\ --output .\output\
 
-dotnet publish --self-contained --runtime win-x64
+dotnet publish --self-contained --runtime win-x64 -p:PublishReadyToRun=true
