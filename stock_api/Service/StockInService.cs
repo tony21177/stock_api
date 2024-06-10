9076a1518c51c5e0847046eaaec69c140b72b55a
tony <tony@ying31.com>
2024-06-10 22:10:29 +0800

    修正acceptance_item的inStockStatus在

--- stock_api/Service/StockInService.cs
+++ stock_api/Service/StockInService.cs
@@ -306,13 +306,13 @@ namespace stock_api.Service
                 }
 
                 // 判斷是否全部驗收完
-                if (existingAcceptanceItem.AcceptQuantity >= existingAcceptanceItem.OrderQuantity)
+                if (existingAcceptanceItem.AcceptQuantity!=null&&existingAcceptanceItem.AcceptQuantity >= existingAcceptanceItem.OrderQuantity)
                 {
                     existingAcceptanceItem.InStockStatus = CommonConstants.PurchaseSubItemReceiveStatus.DONE;
                     existingAcceptanceItem.VerifyAt = DateTime.Now;
                 }
                 // 判斷是否部分驗收
-                if (existingAcceptanceItem.AcceptQuantity >0&& existingAcceptanceItem.AcceptQuantity<existingAcceptanceItem.OrderQuantity)
+                if (existingAcceptanceItem.AcceptQuantity != null && existingAcceptanceItem.AcceptQuantity >0&& existingAcceptanceItem.AcceptQuantity<existingAcceptanceItem.OrderQuantity)
                 {
                     existingAcceptanceItem.InStockStatus = CommonConstants.PurchaseSubItemReceiveStatus.PART;
                     existingAcceptanceItem.VerifyAt = DateTime.Now;
