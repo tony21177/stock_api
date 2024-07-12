f5018eea8e8ca919a2f119c91c377f12288b4e26
Yung-Yu-Lin <wfjh10602@gmail.com>
2024-07-12 16:45:56 +0800

    移除 group 排法

--- stock_api/Service/WarehouseProductService.cs
+++ stock_api/Service/WarehouseProductService.cs
@@ -142,8 +142,7 @@ namespace stock_api.Service
                     "AllowReceiveDateRange" => query.OrderByDescending(h => h.AllowReceiveDateRange),
                     "CreatedAt" => query.OrderByDescending(h => h.CreatedAt),
                     "UpdatedAt" => query.OrderByDescending(h => h.UpdatedAt),
-                    "ProductCode" => query.OrderByDescending(h => h.ProductCode),
-                    "GroupID" => query.OrderByDescending(h => h.GroupNames, StringComparer.OrdinalIgnoreCase),
+                    "ProductCode" => query.OrderByDescending(h => h.ProductCode)
                     _ => query.OrderByDescending(h => h.UpdatedAt),
                 };
             }
@@ -163,8 +162,7 @@ namespace stock_api.Service
                     "AllowReceiveDateRange" => query.OrderBy(h => h.AllowReceiveDateRange),
                     "CreatedAt" => query.OrderBy(h => h.CreatedAt),
                     "UpdatedAt" => query.OrderBy(h => h.UpdatedAt),
-                    "ProductCode" => query.OrderBy(h => h.ProductCode),
-                    "GroupID" => query.OrderBy(h => h.GroupNames, StringComparer.OrdinalIgnoreCase),
+                    "ProductCode" => query.OrderBy(h => h.ProductCode)
                     _ => query.OrderBy(h => h.UpdatedAt),
                 };
             }
