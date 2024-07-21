4e50ce4f90293b219c06b8e7777828e3595491e3
tony <tony@ying31.com>
2024-07-21 22:55:20 +0800

    modify script

--- Changhua_script/start_api.bat
+++ Changhua_script/start_api.bat
@@ -1,15 +1,28 @@
 @echo off
 
 :: 定義源路徑和目標路徑
-set SOURCE_PATH=C:\Users\imsadmin\Desktop\appsettings
 set DESTINATION_PATH=C:\stock_api\output\net6.0\win-x64
 
 :: 刪除原先的appsettings.json文件（如果存在）
-if exist "appsettings.json" del "appsettings.json"
+if exist "%DESTINATION_PATH%\appsettings.json" (
+    del "%DESTINATION_PATH%\appsettings.json"
+    echo appsettings.json deleted
+) else (
+    echo appsettings.json not exist
+)
 
 :: 將appsettings_KimForest.json重命名為appsettings.json
-ren "appsettings_KimForest.json" "appsettings.json"
+if exist "%DESTINATION_PATH%\appsettings_KimForest.json" (
+    ren "%DESTINATION_PATH%\appsettings_KimForest.json" "appsettings.json"
+    echo renamed appsettings_KimForest.json to appsettings.json
+) else (
+    echo appsettings_KimForest.json not exist
+    pause
+    exit /b 1
+)
 
 :: 切換到目標目錄並運行 exe 文件
 cd "%DESTINATION_PATH%"
 start "" "stock_api.exe"
+
+pause
