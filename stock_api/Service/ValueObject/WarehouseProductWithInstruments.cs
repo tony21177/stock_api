3f62bf6db253c9b2df16b262bb53ce3f5eab4366
tony <tony@ying31.com>
2025-02-13 12:34:58 +0800

    update QcType bug

--- stock_api/Service/ValueObject/WarehouseProductWithInstruments.cs
+++ stock_api/Service/ValueObject/WarehouseProductWithInstruments.cs
@@ -2,7 +2,7 @@
 
 namespace stock_api.Service.ValueObject
 {
-    public class WarehouseProductWithInstruments: WarehouseProductStockOutView
+    public class WarehouseProductWithInstruments: WarehouseProduct
     {
         public List<int> InstrumentIdList { get; set; } = new List<int>();
         public List<string> InstrumentNameList { get; set; } = new List<string>();  
