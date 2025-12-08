045f2f44fc30c387645cdbc537138b64b15c1935
tony <tony@ying31.com>
2025-12-08 18:56:53 +0800

    [feat]新舊批號次判定邏輯

--- stock_api/Controllers/QcController.cs
+++ stock_api/Controllers/QcController.cs
@@ -149,17 +149,17 @@ namespace stock_api.Controllers
                             {
                                 lot.IsNewLotNumber = false;
                             }
-                            //var productNewLotNumberBatchList = newLotNumberBatchList.Where(i => i.ProductId == lot.ProductId).Select(m => m.LotNumberBatch).ToList();
-                            //if (!productNewLotNumberBatchList.Contains(lot.LotNumberBatch))
-                            //{
-                            //    lot.IsNewLotNumberBatch = false;
-                            //}
-
-                            // 歷史資料批次以前沒出現過
-                            if (productHistory.Where(i => i.CreatedAt < lot.InStockTime).Any(p => p.LotNumberBatch == lot.LotNumberBatch))
+                            var productNewLotNumberBatchList = newLotNumberBatchList.Where(i => i.ProductId == lot.ProductId).Select(m => m.LotNumberBatch).ToList();
+                            if (!productNewLotNumberBatchList.Contains(lot.LotNumberBatch))
                             {
                                 lot.IsNewLotNumberBatch = false;
                             }
+
+                            // 歷史資料批次以前沒出現過
+                            //if (productHistory.Where(i => i.CreatedAt < lot.InStockTime).Any(p => p.LotNumberBatch == lot.LotNumberBatch))
+                            //{
+                            //    lot.IsNewLotNumberBatch = false;
+                            //}
                         }
                     }
                 }
@@ -359,7 +359,12 @@ namespace stock_api.Controllers
                 {
                     m.IsNewLotNumber = false;
                 }
-                
+                var productNewLotNumberBatchList = newLotNumberBatchList.Where(i => i.ProductId == m.ProductId).Select(m => m.LotNumberBatch).ToList();
+                if (!productNewLotNumberBatchList.Contains(m.LotNumberBatch))
+                {
+                    m.IsNewLotNumberBatch = false;
+                }
+
                 // 新增：前一次入庫的批號 (找出同一產品在當前入庫時間之前的最後一筆入庫紀錄)
                 var productHistory = _stockInService.GetInStockRecordsHistory(m.ProductId, m.CompId);
                 var prev = productHistory.Where(i => i.CreatedAt < m.InStockTime).OrderByDescending(i => i.CreatedAt).FirstOrDefault();
@@ -374,10 +379,10 @@ namespace stock_api.Controllers
                 m.VerifyAt = m.InStockTime;
 
                 // 歷史資料批次以前沒出現過
-                if (productHistory.Where(i=>i.CreatedAt< m.InStockTime).Any(p=>p.LotNumberBatch==m.LotNumberBatch))
-                {
-                    m.IsNewLotNumberBatch = false;
-                }
+                //if (productHistory.Where(i=>i.CreatedAt< m.InStockTime).Any(p=>p.LotNumberBatch==m.LotNumberBatch))
+                //{
+                //    m.IsNewLotNumberBatch = false;
+                //}
 
                 var matchedInStockRecord = inStockRecords.Where(i => i.InStockId == m.InStockId).FirstOrDefault();
                 m.ExpirationDate = matchedInStockRecord?.ExpirationDate;
@@ -466,11 +471,11 @@ namespace stock_api.Controllers
                 {
                     qcMainWithDetailAndFlows.IsNewLotNumber = false;
                 }
-                //var productNewLotNumberBatchList = newLotNumberBatchList.Where(i => i.ProductId == m.ProductId).Select(m => m.LotNumberBatch).ToList();
-                //if (!productNewLotNumberBatchList.Contains(m.LotNumberBatch))
-                //{
-                //    qcMainWithDetailAndFlows.IsNewLotNumberBatch = false;
-                //}
+                var productNewLotNumberBatchList = newLotNumberBatchList.Where(i => i.ProductId == m.ProductId).Select(m => m.LotNumberBatch).ToList();
+                if (!productNewLotNumberBatchList.Contains(m.LotNumberBatch))
+                {
+                    qcMainWithDetailAndFlows.IsNewLotNumberBatch = false;
+                }
 
                 // 新增：前一次入庫的批號 (找出同一產品在當前入庫時間之前的最後一筆入庫紀錄)
                 var productHistory = _stockInService.GetInStockRecordsHistory(qcMainWithDetailAndFlows.ProductId, qcMainWithDetailAndFlows.CompId);
@@ -485,10 +490,10 @@ namespace stock_api.Controllers
                 }
 
                 // 歷史資料批次以前沒出現過
-                if (productHistory.Where(i => i.CreatedAt < m.InStockTime).Any(p => p.LotNumberBatch == m.LotNumberBatch))
-                {
-                    qcMainWithDetailAndFlows.IsNewLotNumberBatch = false;
-                }
+                //if (productHistory.Where(i => i.CreatedAt < m.InStockTime).Any(p => p.LotNumberBatch == m.LotNumberBatch))
+                //{
+                //    qcMainWithDetailAndFlows.IsNewLotNumberBatch = false;
+                //}
 
                 var matchedInStockRecord = inStockRecords.Where(i => i.InStockId==qcMainWithDetailAndFlows.InStockId).FirstOrDefault();
                 qcMainWithDetailAndFlows.ExpirationDate = matchedInStockRecord?.ExpirationDate;
