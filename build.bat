a092243db6d052ec03150aea2af0ade5e7189027
tony <tony@ying31.com>
2026-01-02 22:28:53 +0800

    [feat] build腳本

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @REM dotnet build  --source .\stock_api\ --output .\output\
 
-dotnet publish --self-contained --runtime win-x64 -p:DebugType=Full -p:DebugSymbols=true
+@REM dotnet publish --self-contained --runtime win-x64 -p:DebugType=Full -p:DebugSymbols=true
+
+dotnet publish -c Release --self-contained --runtime win-x64 -p:PublishReadyToRun=true -p:PublishTrimmed=false
